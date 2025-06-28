c10b5edd9ad3cf17413da6aec04a221389ac581f
NADEEN-cash <wadadannena@tutamail.com>
2025-06-28 16:58:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=XE8iTARA#uF_D0J1GQMUJ04s9VM_drv6KGLsI3EPGNE-qhVFlpBI' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=XVUGkZAJ#9s5arXLTJfvV-KSux8oYUFgldUU6f8bmLBPAeNqhPEM' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME,            
 };
